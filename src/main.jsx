40903273c2066010ec418ea8222b75021d989475
Mahbubullah <mahbubullahpathan@gmail.com>
2023-11-05 19:07:15 +0600

    react router dom install

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<div> <h3>route</h3></div>,
+    // errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/contacts",
+        element: <div> <h3>ab</h3></div>,
+      },
+    ],
+  },
+]);
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
