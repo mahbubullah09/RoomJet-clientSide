80dbdf23fb655f66af5c2f7173c226a6b86dd4ff
Mahbubullah <mahbubullahpathan@gmail.com>
2023-11-08 00:53:18 +0600

    booked fuction

--- src/Booking/Bookings.jsx
+++ src/Booking/Bookings.jsx
@@ -3,6 +3,7 @@ import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AuthContext } from "../Provider/authProvider";
 import toast from "react-hot-toast";
+import Swal from "sweetalert2";
 
 const Bookings = () => {
   const { user } = useContext(AuthContext);
@@ -13,6 +14,7 @@ const Bookings = () => {
   const [Name, setname] = useState();
   const [phone, setPhone] = useState();
   const [date, setDate] = useState();
+  const[dataChange,setDataChange] = useState()
 
   useEffect(() => {
     fetch("http://localhost:5000/rooms")
@@ -21,25 +23,129 @@ const Bookings = () => {
     const findData = data?.find((data) => data._id == id);
     SetRoom(findData);
   }, [data, id]);
+  //   const { room_name, price, image, size } = room;
+const room_name = room?.room_name
+const price= room?.price
+const image = room?.image
+const size = room?.size
+const email = user?.email
+const _id = room?._id
 
-  const minDate = moment().format("YYYY-MM-DD");
-  const handleBookings = (e) => {
-    e.preventDefault();
-    const Name = e.target.name.value;
-    setname(Name);
-    const phone = e.target.phone.value;
-    setPhone(phone);
-    const date = e.target.date.value;
-    setDate(date);
-    console.log(Name, phone, date);
+//setToday date
+const todayDate = moment().format("YYYY-MM-DD");
+
+//handleBookings
+const handleBookings = (e) => {
+  e.preventDefault();
+  const Name = e.target.name.value;
+  setname(Name);
+  const phone = e.target.phone.value;
+  setPhone(phone);
+  const date = e.target.date.value;
+  setDate(date);
+  console.log(Name, phone, date);
+
+  console.log(Name,date,phone);
+};
+
+//handledatechange
+
+// const handleDatechange = (e) =>{
+//     e.preventDefault();
+    
+//     // const toggledate = e.target.date.value
+//     console.log(toggledate);
+// }
+
+
+
+
+  //fetch bokked data
+
+  const [booked, setBooked] = useState([]);
+  const url = `http://localhost:5000/booked?room_id=${_id}`;
+
+  useEffect(() => {
+   
+//    axios.get(url, {withCredentials:true})
+//    .then(res => {
+//     setBookings(res.data)
+//    })
+        
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setBooked(data));
+  }, [url]);
+
+  //filter 
+
+  const [alreadyBooked, setAlreadyBooked] = useState();
+  console.log(alreadyBooked);
+
+  useEffect(() => {
+
+    const findBooked = booked.find((data) => data.date === dataChange)
+    setAlreadyBooked(findBooked)
+
+
+  },[dataChange,booked])
+
+
+ 
+
+
+
+
+
+
+
+//post Bookings
+
+  const handleSubmit = () => {
+    console.log("modal data");
+    if (Name && phone && date) {
+      let addBooking = {
+        room_name,
+        price,
+        image,
+        size,
+        room_id:  _id,
+        Name,
+        email,
+        phone,
+        date,
+      };
+      console.log(addBooking);
+
+      fetch('http://localhost:5000/bookings', {
+        method: 'POST',
+        headers:{
+            'content-type' : 'application/json'
+        },
+        body: JSON.stringify(addBooking)
+    })
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data);
+
+        if(data.insertedId){
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Your work has been saved',
+                showConfirmButton: false,
+                timer: 1500
+              })
+
+        }
+       
+    })
+    } else {
+      toast.error("Can't fill blank any input field");
+    }
   };
 
-  const handleSubmit = () =>{
-    if(!Name ) {
-        toast.error("Can't fill blank any input field" )
 
-    } 
-  }
   return (
     <div>
       <h2 className="text-2xl font-semibold text-gray-700 text-center">
@@ -60,7 +166,7 @@ const Bookings = () => {
                     Availability:
                   </span>
                   <span className="text-gray-600 ">
-                    {room?.availability ? <p>Available</p> : <p>Unavailable</p>}
+                    {!alreadyBooked ? <p>Available</p> : <p>Unavailable</p>}
                   </span>
                 </div>
               </div>
@@ -118,17 +224,21 @@ const Bookings = () => {
                     Your Booking Date
                   </label>
                 </div>
-                <input
+                <input 
                   required
                   name="date"
-                  min={minDate}
+                  value={dataChange}
+                  onChange={(e)=>setDataChange(e.target.value)}
+                  min={todayDate}
                   className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
                   type="date"
                 />
               </div>
 
               <div className="mt-8">
-                <button
+                {
+                    !alreadyBooked ?
+                    <button
                   type="submit"
                   onClick={() =>
                     document.getElementById("my_modal_5").showModal()
@@ -137,6 +247,16 @@ const Bookings = () => {
                 >
                   Book Now
                 </button>
+                :
+                <button
+                type="submit"
+                disabled
+               
+                className="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600"
+              >
+               Booked!! Try another date
+              </button>
+                }
               </div>
             </form>
 
@@ -150,13 +270,12 @@ const Bookings = () => {
                 <h3 className="font-bold text-lg">{room?.room_name}</h3>
                 <p className="py-4">Price per night: ${room?.price}</p>
                 <p className="py-4">Room Size: {room?.size}</p>
-                {
-                    room?.specialOffer ?
-                    <p className="py-4">Special Offer: {room?.specialOffer }</p>
-                    :
-                    ''
-                }
-                 <h3 className="text-center text-3xl">Personal Details</h3>
+                {room?.specialOffer ? (
+                  <p className="py-4">Special Offer: {room?.specialOffer}</p>
+                ) : (
+                  ""
+                )}
+                <h3 className="text-center text-3xl">Personal Details</h3>
                 <hr />
                 <p className="py-4">Name: {Name}</p>
                 <p className="py-4">Email: {user?.email}</p>
@@ -165,7 +284,9 @@ const Bookings = () => {
                 <div className="modal-action">
                   <form method="dialog">
                     {/* if there is a button in form, it will close the modal */}
-                    <button onClick={handleSubmit} className="btn">Ok</button>
+                    <button onClick={handleSubmit} className="btn">
+                      Ok
+                    </button>
                   </form>
                 </div>
               </div>
