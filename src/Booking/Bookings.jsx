eb1dc41d7378866bf4c8c70ccad4575188d8a153
Mahbubullah <mahbubullahpathan@gmail.com>
2023-11-07 16:53:16 +0600

    booking toast

--- src/Booking/Bookings.jsx
+++ src/Booking/Bookings.jsx
@@ -1,54 +1,59 @@
 import moment from "moment";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import { AuthContext } from "../Provider/authProvider";
+import toast from "react-hot-toast";
 
 const Bookings = () => {
+  const { user } = useContext(AuthContext);
 
-    const {id} = useParams();
-    const [data, setData] = useState([]);
-    const [room, SetRoom] = useState([]);
-  
-    useEffect(() => {
-      fetch('http://localhost:5000/rooms')
-        .then((res) => res.json())
-        .then((data) => setData(data));
-      const findData = data?.find((data) => data._id == id);
-      SetRoom(findData);
-    }, [data, id]);
+  const { id } = useParams();
+  const [data, setData] = useState([]);
+  const [room, SetRoom] = useState([]);
+  const [Name, setname] = useState();
+  const [phone, setPhone] = useState();
+  const [date, setDate] = useState();
 
-    const minDate = moment().format('YYYY-MM-DD');
-    const handleBookings = (e) =>{
-        e.preventDefault();
-        const name = e.target.name.value;
-        const phone = e.target.phone.value;
-        const date = e.target.date.value;
-        console.log(name,phone,date);
-        
+  useEffect(() => {
+    fetch("http://localhost:5000/rooms")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+    const findData = data?.find((data) => data._id == id);
+    SetRoom(findData);
+  }, [data, id]);
 
+  const minDate = moment().format("YYYY-MM-DD");
+  const handleBookings = (e) => {
+    e.preventDefault();
+    const Name = e.target.name.value;
+    setname(Name);
+    const phone = e.target.phone.value;
+    setPhone(phone);
+    const date = e.target.date.value;
+    setDate(date);
+    console.log(Name, phone, date);
+  };
 
-    }
-  
-    return (
-        <div>
-            
-         
-            <h2 className="text-2xl font-semibold text-gray-700 text-center">Book Your Room Now</h2>
-<div className="py-6 px-6">
-    <div className="px-6 flex flex-col md:flex-row gap-4 bg-white rounded-lg shadow-lg overflow-hidden py-10 mx-auto  lg:max-w-6xl">
-        <div className=" w-full  bg-cover">
-       
+  const handleSubmit = () =>{
+    if(!Name ) {
+        toast.error("Can't fill blank any input field" )
+
+    } 
+  }
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold text-gray-700 text-center">
+        Book Your Room Now
+      </h2>
+      <div className="py-6 px-6">
+        <div className="px-6 flex flex-col md:flex-row gap-4 bg-white rounded-lg shadow-lg overflow-hidden py-10 mx-auto  lg:max-w-6xl">
+          <div className=" w-full  bg-cover">
             <img className="w-full object-cover " src={room?.image} alt="" />
             <div className="md:flex-1 px-4">
-             
               <div className="flex mb-4">
                 <div className="mr-4">
-                  <span className="font-bold text-gray-700 ">
-                    Price:
-                  </span>
-                  <span className="text-gray-600 ">
-                    ${room?.price}
-                  </span>
+                  <span className="font-bold text-gray-700 ">Price:</span>
+                  <span className="text-gray-600 ">${room?.price}</span>
                 </div>
                 <div className="flex ">
                   <span className="font-bold text-gray-700  ">
@@ -61,78 +66,115 @@ const Bookings = () => {
               </div>
               <div className="flex">
                 <p className="text-gray-700 mr-4">Size: {room?.size}</p>
-              {
-                room?.specialOffer ?
-                <p className="text-gray-700">Special Offer : {
-                    room?.specialOffer ?
+                {room?.specialOffer ? (
+                  <p className="text-gray-700">
+                    Special Offer :{" "}
+                    {room?.specialOffer ? (
+                      <span className="text-gray-700">
+                        {room?.specialOffer}
+                      </span>
+                    ) : (
+                      ""
+                    )}
+                  </p>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="w-full ">
+            <h2 className="text-2xl font-bold text-gray-800 ">
+              {room?.room_name}
+            </h2>
+            <p className="text-gray-600  text-sm mb-4">{room?.description}</p>
 
-                    <span className="text-gray-700">{room?.specialOffer }</span>
-                    :
-                    ""
-                    }</p>
+            <form onSubmit={handleBookings}>
+              <div className="mt-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Name
+                </label>
+                <input
+                  required
+                  name="name"
+                  className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+                  type="text"
+                />
+              </div>
+              <div className="mt-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Phone Number
+                </label>
+                <input
+                  required
+                  name="phone"
+                  className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+                  type="text"
+                />
+              </div>
+              <div className="mt-4">
+                <div className="flex justify-between">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Your Booking Date
+                  </label>
+                </div>
+                <input
+                  required
+                  name="date"
+                  min={minDate}
+                  className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+                  type="date"
+                />
+              </div>
+
+              <div className="mt-8">
+                <button
+                  type="submit"
+                  onClick={() =>
+                    document.getElementById("my_modal_5").showModal()
+                  }
+                  className="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600"
+                >
+                  Book Now
+                </button>
+              </div>
+            </form>
+
+            <dialog
+              id="my_modal_5"
+              className="modal modal-bottom sm:modal-middle"
+            >
+              <div className="modal-box">
+                <h3 className="text-center text-3xl">Room Details</h3>
+                <hr />
+                <h3 className="font-bold text-lg">{room?.room_name}</h3>
+                <p className="py-4">Price per night: ${room?.price}</p>
+                <p className="py-4">Room Size: {room?.size}</p>
+                {
+                    room?.specialOffer ?
+                    <p className="py-4">Special Offer: {room?.specialOffer }</p>
                     :
                     ''
-              }
-              </div>
-              </div>
-        </div>
-        <div className="w-full ">
-            
-
-
-            
-            <h2 className="text-2xl font-bold text-gray-800 ">
-                {room?.room_name}
-              </h2>
-              <p className="text-gray-600  text-sm mb-4">
-                {room?.description}
-              </p>
-
-
-       <form onSubmit={handleBookings}>
-       <div className="mt-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">Name</label>
-                <input required name='name' className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="text" />
-            </div>
-            <div className="mt-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">Phone Number</label>
-                <input required name="phone" className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="text" />
-            </div>
-            <div className="mt-4">
-                <div className="flex justify-between">
-                    <label className="block text-gray-700 text-sm font-bold mb-2">Your Booking Date</label>
-                    
+                }
+                 <h3 className="text-center text-3xl">Personal Details</h3>
+                <hr />
+                <p className="py-4">Name: {Name}</p>
+                <p className="py-4">Email: {user?.email}</p>
+                <p className="py-4">Phone: {phone}</p>
+                <p className="py-4">Booking Date: {date}</p>
+                <div className="modal-action">
+                  <form method="dialog">
+                    {/* if there is a button in form, it will close the modal */}
+                    <button onClick={handleSubmit} className="btn">Ok</button>
+                  </form>
                 </div>
-                <input required name="date" min={minDate} className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="date" />
-            </div>
-          
-            <div className="mt-8">
-                <button type="submit"   className="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">Book Now</button>
-            </div>
-       </form>
-
-
-            <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-  <div className="modal-box">
-    <h3 className="font-bold text-lg">Hello!</h3>
-    <p className="py-4">Press ESC key or click the button below to close</p>
-    <div className="modal-action">
-      <form method="dialog">
-        {/* if there is a button in form, it will close the modal */}
-        <button className="btn">Close</button>
-      </form>
-    </div>
-  </div>
-</dialog>
-           
+              </div>
+            </dialog>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-
-
-
-        </div>
-    );
+  );
 };
 
 export default Bookings;
--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Footer/Footer';
+import { Toaster } from 'react-hot-toast';
 
 const MainLayout = () => {
     return (
@@ -9,6 +10,7 @@ const MainLayout = () => {
             <Navbar/>
             <Outlet/>
             <Footer/>
+            <Toaster/>
         </div>
     );
 };
