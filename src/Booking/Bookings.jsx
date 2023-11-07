99d468e87defd4964d9a00a2e686c61b5264e578
Mahbubullah <mahbubullahpathan@gmail.com>
2023-11-07 15:05:11 +0600

    update bookings design

--- src/Booking/Bookings.jsx
+++ src/Booking/Bookings.jsx
@@ -76,6 +76,10 @@ const Bookings = () => {
                 {room?.description}
               </p>
             <div className="mt-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2">Name</label>
+                <input className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="text" />
+            </div>
+            <div className="mt-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2">Phone Number</label>
                 <input className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="text" />
             </div>
@@ -86,13 +90,7 @@ const Bookings = () => {
                 </div>
                 <input className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="date" />
             </div>
-            <div className="mt-4">
-                <div className="flex justify-between">
-                    <label className="block text-gray-700 text-sm font-bold mb-2">Your Checkout Date</label>
-                    
-                </div>
-                <input className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="date" />
-            </div>
+          
             <div className="mt-8">
                 <button className="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">Book Now</button>
             </div>
