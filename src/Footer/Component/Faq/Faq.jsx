23bb5730dd71679b9f4a612672b39d65ef98eae1
Mahbubullah <mahbubullahpathan@gmail.com>
2023-11-08 19:44:38 +0600

    faq update

--- src/Footer/Component/Faq/Faq.jsx
+++ src/Footer/Component/Faq/Faq.jsx
@@ -1,86 +1,84 @@
-
+import { Helmet } from "react-helmet-async";
 
 const Faq = () => {
-    return (
-        <div>
-          <div className="faq-cont flex flex-col-reverse gap-8 my-8 lg:flex lg:flex-row justify-between items-center">
-            <div className="left-cont md:w-1/2">
-              <div
-                tabindex="0"
-                className="my-4 text-black collapse collapse-plus border border-base-300 bg-[#a4f1a8]"
-              >
-                <div className="collapse-title text-xl font-medium">
-                  How does FoodHub work?
-                </div>
-                <div className="collapse-content">
-                  <p>
-                    FoodHub is a user-friendly platform that allows you to explore
-                    and order from a variety of restaurants and cuisines. Simply
-                    download our app or visit our website, browse the menu, select
-                    your dishes, and complete your order. We'll take care of the
-                    rest.{" "}
-                  </p>
-                </div>
-              </div>
-              <div
-                tabindex="0"
-                className="my-4 text-black collapse collapse-plus border border-base-300 bg-[#a4f1a8]"
-              >
-                <div className="collapse-title text-xl font-medium">
-                  What should I do if there's an issue with my order or delivery?
-                </div>
-                <div className="collapse-content">
-                  <p>
-                    If you encounter any problems with your order, such as missing
-                    items or issues with the quality of your meal, please contact
-                    our customer support team. We're here to assist you and will
-                    work to resolve the issue promptly. You can find our contact
-                    information on the website or app. Your satisfaction is our
-                    priority!
-                  </p>
-                </div>
-              </div>
-              <div
-                tabindex="0"
-                className="my-4 text-black collapse collapse-plus border border-base-300 bg-[#a4f1a8]"
-              >
-                <div className="collapse-title text-xl font-medium">
-                  What is the delivery time for my order?
-                </div>
-                <div className="collapse-content">
-                  <p>
-                    Delivery times may vary depending on your location and the
-                    restaurant's availability. You can usually find estimated
-                    delivery times displayed during the ordering process. Rest
-                    assured, we strive to deliver your food as quickly as possible
-                    while ensuring it's hot and fresh.
-                  </p>
-                </div>
-              </div>
-              <div
-                tabindex="0"
-                className="my-4 text-black collapse collapse-plus border border-base-300 bg-[#a4f1a8]"
-              >
-                <div className="collapse-title text-xl font-medium">
-                  What payment methods are accepted?
-                </div>
-                <div className="collapse-content">
-                  <p>
-                    We accept various payment methods, including credit and debit
-                    cards, digital wallets, and in some cases, cash upon delivery.
-                    You can find the available payment options during the checkout
-                    process for each order.
-                  </p>
-                </div>
-              </div>
+  return (
+    <div>
+        <Helmet>
+        <title>RoomJet-FAQ</title>
+        </Helmet>
+      <div className="faq-cont flex flex-col-reverse gap-8 my-8 lg:flex lg:flex-row justify-between items-center">
+        <div className="left-cont md:w-1/2">
+          <div
+            tabindex="0"
+            className="my-4 text-black collapse collapse-plus border border-base-300 bg-[#a4f1a8]"
+          >
+            <div className="collapse-title text-xl font-medium">
+              Is RoomJet Hotel a trusted place to stay
             </div>
-    
-            <div className="right-cont">
-              <img src="https://i.ibb.co/1zbr0gJ/faq.png" alt="" />
+            <div className="collapse-content">
+              <p>
+                RoomJet Hotel is committed to delivering exceptional guest
+                experiences. We uphold high standards of cleanliness,
+                hospitality, and service, ensuring that your stay is enjoyable
+                and worry-free.
+              </p>
+            </div>
+          </div>
+          <div
+            tabindex="0"
+            className="my-4 text-black collapse collapse-plus border border-base-300 bg-[#a4f1a8]"
+          >
+            <div className="collapse-title text-xl font-medium">
+              What amenities and services does RoomJet Hotel offer?
+            </div>
+            <div className="collapse-content">
+              <p>
+                RoomJet Hotel provides a range of amenities and services,
+                including complimentary breakfast, free Wi-Fi, room service, and
+                more. You can find specific details on our website or by
+                contacting us directly.
+              </p>
+            </div>
+          </div>
+          <div
+            tabindex="0"
+            className="my-4 text-black collapse collapse-plus border border-base-300 bg-[#a4f1a8]"
+          >
+            <div className="collapse-title text-xl font-medium">
+              Can I modify or cancel my RoomJet Hotel reservation?
+            </div>
+            <div className="collapse-content">
+              <p>
+                Yes, you can modify or cancel your reservation in accordance
+                with our cancellation policy. Review the specific terms on your
+                booking confirmation or contact our customer support for
+                assistance.
+              </p>
+            </div>
+          </div>
+          <div
+            tabindex="0"
+            className="my-4 text-black collapse collapse-plus border border-base-300 bg-[#a4f1a8]"
+          >
+            <div className="collapse-title text-xl font-medium">
+              Is my personal information safe when I book with RoomJet Hotel?
+            </div>
+            <div className="collapse-content">
+              <p>
+                Your privacy and data security are a top priority for us.
+                RoomJet Hotel uses secure, encrypted connections and follows
+                industry best practices to protect your information.
+              </p>
             </div>
           </div>
         </div>
-      );
-    };
+
+        <div className="right-cont">
+          <img src="https://i.ibb.co/1zbr0gJ/faq.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Faq;
