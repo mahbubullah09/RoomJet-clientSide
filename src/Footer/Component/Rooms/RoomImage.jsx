39542e625c73a578190e24b0fee6ff7eb179da50
Mahbubullah <mahbubullahpathan@gmail.com>
2023-11-09 16:31:49 +0600

    room image route

--- src/Footer/Component/Rooms/RoomImage.jsx
+++ src/Footer/Component/Rooms/RoomImage.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const RoomImage = ({data}) => {
-    return (
+const RoomImage = ({ data }) => {
+  return (
+    <div>
+      <Link to={`/rooms/${data?._id}`}>
         <div>
-        <Link to={`/${data.room_name}`}>
-        <div>
-        <img className='relative w-full' src={data.image} alt="" />
-        < p className='absolute top-0 left-1 md:text-4xl font-bold bg-[#ffcf00] rounded-full py-2 px-2 bg-opacity-40 '>{data.room_name}</p>
+          <img className="relative w-full" src={data?.image} alt="" />
+          <p className="absolute top-0 left-1 md:text-4xl font-bold bg-[#ffcf00] rounded-full py-2 px-2 bg-opacity-40 ">
+            {data?.room_name}
+          </p>
         </div>
-        </Link>
-        </div>
-    );
+      </Link>
+    </div>
+  );
 };
 
 export default RoomImage;
